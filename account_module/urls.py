63f608e1e33121726aab7b391055403287f68c4b
Hamidreza-mohamadi <hamidreza6mohamadi@gmail.com>
2026-09-22 23:25:40 +0330

    feat(auth): complete JWT authentication backend

--- account_module/urls.py
+++ account_module/urls.py
@@ -1,19 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from account_module import views
 
 urlpatterns = [
-    # path('sign-up/', views.SignUp.as_view(), name='sign-in'),
-    # path('login/', views.LogIn.as_view(), name='login'),
-    # path('active-account/<code>/', views.ActiveAccount.as_view(), name='active-account'),
-    # path('logout/', views.LogOut.as_view(), name='logout'),
-    # path('forget-pass/', views.ForgetPassword.as_view(), name='forget-pass'),
-    # path('rewrite-pass/<code>/', views.RewritePass.as_view(), name='rewrite-password'),
-    # path('change-pass/', views.ChangePassword.as_view(), name='change-pass'),
-    # path('edit-profile/', views.EditProfile.as_view(), name='edit-profile'),
-    path('me/', views.CurrentUserView.as_view(), name='sign-in'),
-    # path('user/', views.CurrentUserView.as_view(), name='current-user'),
-    path('login/', views.LoginView.as_view(), name='api-login'),
-    path('logout/', views.LogoutView.as_view(), name='api-logout'),
-
+    path("me/", views.CurrentUserView.as_view(), name="current-user"),
+    path("login/", views.LoginView.as_view(), name="api-login"),
+    path("token/", views.LoginView.as_view(), name="api-token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="api-token-refresh"),
+    path("register/", views.RegisterView.as_view(), name="api-register"),
+    path("logout/", views.LogoutView.as_view(), name="api-logout"),
 ]
