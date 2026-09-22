a5d0072631292f09297526f748a1024889956608
Hamidreza-mohamadi <hamidreza6mohamadi@gmail.com>
2026-09-22 23:25:33 +0330

    feat(auth): complete JWT authentication backend

--- account_module/serializers.py
+++ account_module/serializers.py
@@ -5,7 +5,40 @@ from account_module.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar",
+            "about_user",
+        )
+        read_only_fields = ("id",)
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
 
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+        )
+
+    def validate_username(self, value):
+        if User.objects.filter(username__iexact=value).exists():
+            raise serializers.ValidationError("این نام کاربری قبلاً استفاده شده است.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("این ایمیل قبلاً استفاده شده است.")
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
